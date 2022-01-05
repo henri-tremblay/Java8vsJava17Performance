4910a2a275266ab8ce76351089bd14a78ac20461
Henri Tremblay <henri.tremblay@gmail.com>
2022-01-04 22:53:46 -0500

    Add JMH

---
+++ src/main/java/com/acme/benchmark/MyBenchmark.java
@@ -0,0 +1,35 @@
+package com.acme.benchmark;
+
+import java.util.concurrent.TimeUnit;
+
+import com.acme.performance.Main;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+@Fork(2)
+@Warmup(iterations = 10, time = 2)
+@Measurement(iterations = 10, time = 2)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+public class MyBenchmark {
+
+    private long n = 40;
+
+    @Benchmark
+    public long fibSeq() {
+        return Main.fibonacci(n);
+    }
+
+    @Benchmark
+    public long fibPar() {
+        return new Main.Fibonacci(n).compute();
+    }
+}
